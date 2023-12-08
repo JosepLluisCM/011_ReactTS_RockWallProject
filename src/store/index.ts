b1ea356947ced96c7ede13c04386c76cd0367e75
JosepLluisCM <webdevJosep@gmail.com>
2023-12-08 19:42:01 +0100

    fwfsf

--- src/store/index.ts
+++ src/store/index.ts
@@ -31,5 +31,10 @@ const store = configureStore({
   reducer: loginSlice.reducer,
 });
 
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
 export const loginActions = loginSlice.actions;
 export default store;
